3c2bb4959edf9a9833e511877bced77028a2d0a9
VENKAT6912 <vibecoder18@gmail.com>
2025-09-17 20:13:54 +0530

    fixed hero images and captions

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './Home.css';
 import Navbar from '../../Navbar/Navbar';
+import hero_banner from '../../../assets/hero_banner.jpg'
+import hero_title from '../../../assets/hero_title.png'
 
 const Home = () => {
   return (
     <div className='home'>
         <Navbar/>
-
+        <div className="hero">
+            <img src={hero_banner} alt=""  className='banner-img'/>
+            <div className="hero-caption">
+                <img src={hero_title}alt="" className='caption-img' />
+                <p>
+                    Walk in the footsteps of a lone traveler through the bustling streets of ancient Istanbul, where empires clashed, markets thrived, and every stone tells the story of a city that shaped history.
+                </p>
+            </div>
+        </div>
         
       
     </div>
