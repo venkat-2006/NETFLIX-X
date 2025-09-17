fabd4babbfaf5e88f6c0ec2fc75bf542b5dd6212
VENKAT6912 <vibecoder18@gmail.com>
2025-09-17 21:56:53 +0530

    added new cards on starting page

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import'./TitleCards.css'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = () => {
   return (
     <div className='titlecards'>
-      
+      <h2>Popular on Netflix</h2>
+      <div className="cards-list">
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt={card.name} />
+              <p>{card.name}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
