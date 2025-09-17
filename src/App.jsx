c46e6b5e85bfe32d9bc5c28ea64f96a18f152d12
VENKAT6912 <vibecoder18@gmail.com>
2025-09-17 19:33:25 +0530

    navbar skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Home from './components/pages/Home/Home'
+
 
 const App = () => {
   return (
     <div>
+      <Home/>
       
     </div>
   )
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Home.css';
+import Navbar from '../../Navbar/Navbar';
 
 const Home = () => {
   return (
     <div className='home'>
+        <Navbar/>
+
+        
       
     </div>
   )
