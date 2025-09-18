24c51ee1c38655692522e57e05f02b17110ff743
VENKAT6912 <vibecoder18@gmail.com>
2025-09-19 01:09:13 +0530

    added navigation of pages acc to user using auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './components/pages/Home/Home'
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route, useNavigate } from 'react-router-dom'
 import Login from "./components/pages/Login/Login"
 import Player from './components/pages/Player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
 
 
 const App = () => {
+
+  const navigate=useNavigate();
+
+
+
+
+  useEffect(()=>{
+   onAuthStateChanged(auth,async(user)=>{
+    if(user){
+      console.log("Logged In");
+      navigate('/');
+    }else{
+      console.log("Logged Out");
+      navigate('/login');
+    }
+   })
+  },[])
+
+
+
   return (
     <div>
       <Routes>
