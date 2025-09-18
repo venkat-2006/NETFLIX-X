145a927a93dbaa8428802264b0a6837a534b5910
VENKAT6912 <vibecoder18@gmail.com>
2025-09-19 00:44:53 +0530

    added user sign up function using firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +19,22 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth=getAuth(app);
 const db=getFirestore(app);
+
+const signup=async(name,email,password)=>{
+    try{
+       const res= await createUserWithEmailAndPassword(auth,email,password);
+       const user=res.user;
+       await addDoc(collection(db,"user"),{
+        uid: user.uid,
+        name,
+        authProvider:"local",
+        
+       });
+
+    }catch(error){
+        console.log(error);
+        alert(error);
+
+    }
+
+}
