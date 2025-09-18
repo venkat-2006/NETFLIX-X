674620f0887c89860c90e5910d7f249928d90453
VENKAT6912 <vibecoder18@gmail.com>
2025-09-19 00:39:54 +0530

    added firebase skeleton for auth and db

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6sAwhb-ZP2qsbP6olSATeowzy7foaZ7Y",
+  authDomain: "netflix-x-2cadc.firebaseapp.com",
+  projectId: "netflix-x-2cadc",
+  storageBucket: "netflix-x-2cadc.firebasestorage.app",
+  messagingSenderId: "948827103028",
+  appId: "1:948827103028:web:7217b6ded165d0223f0f61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth=getAuth(app);
+const db=getFirestore(app);
