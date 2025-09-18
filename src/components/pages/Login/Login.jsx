49ce6f7fa6d6f8d1050eede3a05a35ed0201bd24
VENKAT6912 <vibecoder18@gmail.com>
2025-09-19 01:31:46 +0530

    added loading icon/gif while loading after auth

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -2,23 +2,30 @@ import React,{useState} from 'react'
 import './Login.css'
 import logo from '../../../assets/logo.png'
 import { login,signup } from '../../../firebase'
+import netflix_spinner from '../../../assets/netflix_spinner.gif'
 
 const Login = () => {
   const[signState,setSignState]=useState("Sign In");
   const[name,setName]=useState("");
   const[email,setEmail]=useState("");
   const[password,setPassword]=useState("");
+  const[isLoading,setIsLoading]=useState(false);
 
   const user_auth=async(event)=>{
     event.preventDefault();
+    setIsLoading(true);
     if(signState==="Sign In"){
       await login(email,password);
     }else{
       await signup(name,email,password);
     }
+    setIsLoading(false);
   }
 
   return (
+    isLoading?<div className="login-spinner">
+      <img src={netflix_spinner} alt="" />
+    </div>:
     <div className='login'>
       <img src={logo} className='login-logo' alt="" />
       <div className="login-form">
