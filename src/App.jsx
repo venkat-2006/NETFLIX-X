fb69be4977dd97f51186cc85fe9ad149331e864a
VENKAT6912 <vibecoder18@gmail.com>
2025-09-18 15:07:00 +0530

    installed routes and updated the paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Home from './components/pages/Home/Home'
+import { Routes,Route } from 'react-router-dom'
+import Login from "./components/pages/Login/Login"
 
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/login'element={<Login/>}/>
+      </Routes>
+     
       
     </div>
   )
